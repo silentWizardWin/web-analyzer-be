d577fd670cb44ff505c8e86551dd6808e8c61396
Pushpitha Somathilaka <pushpitha.somathilaka@harver.com>
2025-05-10 11:22:48 +0530

    added initial files

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, Go!")
+
+    // analyze - analyze the web page
+    // get html page and parse it
+    // use methods in utils
+    // insert parsed data into util methods
+}
